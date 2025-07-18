cf18369d64889c9651757e43d4def396e4194f56
M Abdullah <insight.abdullah@gmail.com>
2025-07-19 01:59:56 +0500

    implement companionCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,46 @@
-import { Button } from "@/components/ui/button";
+import CTA from "@/components/CTA";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionList from "@/components/CompanionList";
+import { recentSessions } from "@/constants";
 import React from "react";
 
 const Page = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <Button>Click me</Button>
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      <section className="home-section">
+          <CompanionCard 
+          id="123" 
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6a"/>
+           <CompanionCard 
+          id="456" 
+          name="Verba The vocabolury builder"
+          topic="Verbal Communication"
+          subject="science"
+          duration={45}
+          color="#fda234"/>
+           <CompanionCard 
+          id="654" 
+          name="Neura the Brainy Fucker"
+          topic="Neural Network of the Testicles"
+          subject="science"
+          duration={45}
+          color="#fd3a2b"/>
+      </section>
+      
+      <section className="home-section">
+        <CompanionList 
+        title="Recently Completed Sessions"
+        companions={recentSessions}
+        classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA/>
+      </section>
+    </main>
   )
 }
 
